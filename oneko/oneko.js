4254163ca0455603996c951c03092a758bba2d0f
castluno <castluno@xyecoc.com>
2026-02-03 12:46:47 +0700

    Update oneko.js

--- oneko/oneko.js
+++ oneko/oneko.js
@@ -96,7 +96,7 @@
     nekoEl.style.top = `${nekoPosY - 16}px`;
     nekoEl.style.zIndex = 2147483647;
 
-    let nekoFile = "/oneko/oneko.gif"
+    let nekoFile = "https://raw.githubusercontent.com/adryd325/oneko.js/main/oneko.gif"
     const curScript = document.currentScript
     if (curScript && curScript.dataset.cat) {
       nekoFile = curScript.dataset.cat
